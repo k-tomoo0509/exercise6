7f1e81e52cedd3b813311b4c719913a8c079f3ff
k-tomoo0509 <77222789+k-tomoo0509@users.noreply.github.com>
2021-01-17 21:30:27 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Looping through images */
+for(let i = 1;i<=5;i++){
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', "images/pic"+i+".jpg");
+    thumbBar.appendChild(newImage);
+    newImage.onclick = function(a){
+        displayedImage.src = a.target.src;
+    }
+}
+
+/* Wiring up the Darken/Lighten button */
+btn.onclick = function(){
+    const btnClass = getAtrribute("class");
+    if(btnClass === "dark"){
+        btnClass = setAtrribute("class","light");
+        btn.textContent = "Lighten";
+        overlay.style.backgroundColor = rgba(0,0,0,0.5);
+    }else{
+        btnClass = setAtrribute("class","dark");
+        btn.textContent = "Darken";
+        overlay.style.backgroundColor = rgba(0,0,0,0);
+    }
+}
